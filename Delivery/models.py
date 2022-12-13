628328a5b4b0858b06e9c3c2f237c223cbd5e49b
Ziyad <Ziyad@MacBook-Air.local>
2022-12-13 19:35:26 +0300

    Add and edit package for employees

--- Delivery/models.py
+++ Delivery/models.py
@@ -10,20 +10,24 @@ class Customer(models.Model):
 # Package model
 class Package(models.Model):
     #primary key
-    package_number = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     weight = models.FloatField()
     destination = models.CharField(max_length=100)
-    dimensions = models.CharField(max_length=100)
+    dimensions = models.CharField(max_length=100, default='Small')
     insurance_amount = models.FloatField()
-    status = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    value = models.FloatField()
+    status = models.CharField(max_length=100, default='In Transit')
+    category = models.CharField(max_length=100, default='Regular')
+    value = models.FloatField(default=0)
     final_delivery_date = models.DateField()
 
     #derived attributes
     @property
     def payment(self):
         return self.value * (self.weight * 0.1)
+    
+    #string representation
+    def __str__(self):
+        return f"{self.package_number}: {self.weight} {self.destination} {self.dimensions} {self.insurance_amount} {self.status} {self.category} {self.value} {self.final_delivery_date}"
 
 # Retail model
 class RetailCenter(models.Model):
@@ -32,7 +36,7 @@ class RetailCenter(models.Model):
     type = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
     #foreign key
-    package_number = models.ForeignKey(Package, on_delete=models.CASCADE)
+    package_number = models.ForeignKey(Package, on_delete=models.CASCADE, null=True)
 
 # Transportation event model
 class TransportationEvent(models.Model):
@@ -44,13 +48,13 @@ class TransportationEvent(models.Model):
 # Transported By relationship model
 class TransportedBy(models.Model):
     #foreign key
-    package_number = models.ForeignKey(Package, on_delete=models.CASCADE)
-    schedule_number = models.ForeignKey(TransportationEvent, on_delete=models.CASCADE)
+    package_number = models.ForeignKey(Package, on_delete=models.CASCADE, null=True)
+    schedule_number = models.ForeignKey(TransportationEvent, on_delete=models.CASCADE, null=True)
 
 # Locations model
 class Location(models.Model):
     #primary key
-    location_number = models.AutoField(primary_key=True)
+    location_number = models.AutoField(primary_key=True, default=1)
     #foreign key
     #one to many
     package = models.ForeignKey(Package, on_delete=models.CASCADE)
@@ -58,28 +62,28 @@ class Location(models.Model):
 # Trucks part of Location model
 class Truck(models.Model):
     #primary key
-    truck_number = models.AutoField(primary_key=True)
+    truck_number = models.AutoField(primary_key=True, default=1)
     #foreign key
     #one to many
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 # Planes part of Location model
 class Plane(models.Model):
     #primary key
-    flight_number = models.AutoField(primary_key=True)
+    flight_number = models.AutoField(primary_key=True, default=123)
     #foreign key
     #one to many
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 # Airports part of Location model
 class Airport(models.Model):
     #primary key
-    airport_code = models.CharField(max_length=100, primary_key=True)
+    airport_code = models.CharField(max_length=100, primary_key=True, default='ABC')
     #foreign key
     #one to many
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
 # Warehouse part of Location model
 class Warehouse(models.Model):
     #primary key
-    warehouse_address = models.CharField(max_length=100, primary_key=True)
+    warehouse_address = models.CharField(max_length=100, primary_key=True, default='123 Main St')
     #foreign key
     #one to many
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
--- Delivery/static/Delivery/h.js
+++
@@ -1,9 +0,0 @@
-
-function redirect(type) {
-    if (type === "C") {
-        window.location.href = "{% url 'register_customers' %}";
-    }
-    else {
-        window.location.href = "{% url 'register_employee' %}";
-    }        
-}
